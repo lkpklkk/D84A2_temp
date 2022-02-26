e44d964f7fca820caead779e8e2d3cb2c4a3152d
keping le <kepingle@kepings-MacBook-Pro.local>
2022-02-26 00:42:24 -0500

    finished alpha beta pruning

--- MiniMax_search.c
+++ MiniMax_search.c
@@ -740,7 +740,7 @@ double utility(int cat_loc[10][2], int cheese_loc[10][2], int mouse_loc[1][2], i
 int blockedNode = openess(mouse_loc,gr);
  double distanceToCheese = getDisToCheese(mouse_loc,cheese_loc,cheeses);
  double distanceToCats = getAvgCatDis(cat_loc,cats,mouse_loc);
- return -search(gr,cat_loc,cats,cheese_loc,cheeses,mouse_loc)-blockedNode*4+distanceToCheese + 2*distanceToCats + random() % 3;   // <--- Obviously, this will be replaced by your computer utilities
+ return -search(gr,cat_loc,cats,cheese_loc,cheeses,mouse_loc)-blockedNode*4-distanceToCheese + distanceToCats + random() % 3;   // <--- Obviously, this will be replaced by your computer utilities
 }
 
 int checkForTerminal(int mouse_loc[1][2],int cat_loc[10][2],int cheese_loc[10][2],int cats,int cheeses)
