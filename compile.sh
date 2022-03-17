b5c7bf692efe1651a94bf80d9d2116b11405d6cd
keping le <kepingle@kepings-MacBook-Pro.local>
2022-03-17 17:08:23 -0400

    A3

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-g++ -O3 -c -g MiniMax_search.c
-g++ -O3 -g *.o -lm -lglut -lGL -lGLU -lX11 -o MiniMax_search
+g++ -fPIC -O3 -c -g QLearn.c
+g++ -fPIC -O3 -g *.o -lm -lglut -lGL -lGLU -lX11 -o QLearn
 
